5af41b811da0905aae94a5db6046eb207e6509b3
DEVADA VIRPAL SINH <77virpalsinh77@gmail.com>
2025-08-07 00:02:01 +0530

    css for card and buyers

--- src/components/BuyerSeller/Buyer.jsx
+++ src/components/BuyerSeller/Buyer.jsx
@@ -5,28 +5,28 @@ function Buyer() {
   const [data, setdata] = useState([
     {
       imageurl:
-        "https://fastly.picsum.photos/id/296/500/500.jpg?hmac=rBOzu8mvOZGUCyndcJPEmZzwmm_cPMgHHVOma9uAbhk",
+        "https://images.unsplash.com/photo-1483935254693-d16df5d8741a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHdoaXRlc2FoZGV8ZW58MHx8MHx8fDA%3D",
       title: "Card 1 Rendered",
       content:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi sapiente deleniti illo optio qui eaque, tenetur consectetur, dicta cumque accusamus quis dolore possimus eum vel.",
     },
     {
       imageurl:
-        "https://fastly.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0",
+        "https://images.unsplash.com/photo-1528750164675-112c3f715fd9?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHdoaXRlc2FoZGV8ZW58MHx8MHx8fDA%3D",
       title: "Card 2 Rendered",
       content:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi sapiente deleniti illo optio qui eaque, tenetur consectetur, dicta cumque accusamus quis dolore possimus eum vel.",
     },
     {
       imageurl:
-        "https:www.shutterstock.com/image-vector/set-packaging-rectangular-boxes-mockups-side-2466739731",
+        "https://images.unsplash.com/photo-1508784411316-02b8cd4d3a3a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHdoaXRlc2FoZGV8ZW58MHx8MHx8fDA%3D",
       title: "Card 3 Rendered",
       content:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi sapiente deleniti illo optio qui eaque, tenetur consectetur, dicta cumque accusamus quis dolore possimus eum vel.",
     },
     {
       imageurl:
-        "https://www.shutterstock.com/image-photo/close-lab-coat-doctor-pharmacy-medical-2308292631",
+        "https://images.unsplash.com/photo-1584928585752-4a7e7acbb08b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8d2hpdGVzYWhkZXxlbnwwfHwwfHx8MA%3D%3D",
       title: "Card 4 Rendered",
       content:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi sapiente deleniti illo optio qui eaque, tenetur consectetur, dicta cumque accusamus quis dolore possimus eum vel.",
@@ -35,6 +35,20 @@ function Buyer() {
 
   return (
     <>
+    <div>
+      <div className="Buyer_first_section">
+        <div className="Buyer_first_content">
+            <div className="Buyer_first_title">
+              <h1>Buying Pharmaceutical and Healthcare Products Made Easy</h1>
+            </div>
+            <p>Browse, Inquire & Secure Your Deals Seamlessly.</p>
+            <button className="Buyer_button_start">Get started</button>
+        </div>
+        <div className="Buyer_first_image">
+          <img src="https://media.istockphoto.com/id/1281242223/photo/a-white-dove-sits-on-the-first-snow.webp?a=1&b=1&s=612x612&w=0&k=20&c=6hTkF29p5av6YMGoqEXkn7Q4HAoRh29uKW1K_c1Ix0c=" alt="" />
+        </div>
+      </div>
+      <h2 className="buyer_guid_title">Buyers's Guide</h2>
       <div className="Buyer_all_card">
         {data.map((data, ind) => {
           return (
@@ -48,6 +62,7 @@ function Buyer() {
           );
         })}
       </div>
+    </div>
     </>
   );
 }
